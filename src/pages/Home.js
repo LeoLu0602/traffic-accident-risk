2efb41493f5ed0d5e40a7404f20f6430256635c3
Kenny Cartman <lyuholu@gmail.com>
2024-04-11 19:20:52 +0800

    fix: refactor code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,433 +6,189 @@ import {
   TrafficLayer,
   DirectionsRenderer,
 } from '@react-google-maps/api';
+import { simplifiedWeather } from '../simplifiedWeather';
 import Result from '../components/Result';
+import { resolvePath } from 'react-router-dom';
 
 function Home() {
   const [isLoading, setIsLoading] = useState(false);
-  const [centerLat, setCenterLat] = useState(22.9968);
-  const [centerLng, setCenterLng] = useState(120.2169);
-  const [lat1, setLat1] = useState();
-  const [lng1, setLng1] = useState();
-  const [lat2, setLat2] = useState();
-  const [lng2, setLng2] = useState();
+  const [centerLat, setCenterLat] = useState(22.9968); // NCKU's lat == 22.9968
+  const [centerLng, setCenterLng] = useState(120.2169); // NCKU's lng == 120.2169
   const [searchText1, setSearchText1] = useState('');
   const [searchText2, setSearchText2] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [directions, setDirections] = useState(null);
 
-  const simplifiedWeather = {
-    晴天: '晴',
-    晴時多雲: '晴',
-    多雲時晴: '晴',
-    多雲: '陰',
-    多雲時陰: '陰',
-    陰時多雲: '陰',
-    陰天: '陰',
-    多雲陣雨: '暴雨',
-    多雲短暫雨: '雨',
-    多雲短暫陣雨: '暴雨',
-    午後短暫陣雨: '暴雨',
-    短暫陣雨: '雨',
-    多雲時晴短暫陣雨: '暴雨',
-    多雲時晴短暫雨: '暴雨',
-    晴時多雲短暫陣雨: '暴雨',
-    晴短暫陣雨: '暴雨',
-    短暫雨: '暴雨',
-    多雲時陰短暫雨: '暴雨',
-    多雲時陰短暫陣雨: '暴雨',
-    陰時多雲短暫雨: '暴雨',
-    陰時多雲短暫陣雨: '暴雨',
-    雨天: '雨',
-    晴午後陰短暫雨: '雨',
-    晴午後陰短暫陣雨: '暴雨',
-    陰短暫雨: '雨',
-    陰短暫陣雨: '暴雨',
-    陰午後短暫陣雨: '暴雨',
-    多雲時陰有雨: '雨',
-    多雲時陰陣雨: '暴雨',
-    晴時多雲陣雨: '暴雨',
-    多雲時晴陣雨: '暴雨',
-    陰時多雲有雨: '雨',
-    陰時多雲有陣雨: '暴雨',
-    陰時多雲陣雨: '暴雨',
-    陰有雨: '雨',
-    陰有陣雨: '暴雨',
-    陰雨: '雨',
-    陰陣雨: '暴雨',
-    陣雨: '暴雨',
-    午後陣雨: '暴雨',
-    有雨: '雨',
-    多雲陣雨或雷雨: '暴雨',
-    多雲短暫陣雨或雷雨: '暴雨',
-    多雲短暫雷陣雨: '暴雨',
-    多雲雷陣雨: '暴雨',
-    短暫陣雨或雷雨後多雲: '暴雨',
-    短暫雷陣雨後多雲: '暴雨',
-    短暫陣雨或雷雨: '暴雨',
-    晴時多雲短暫陣雨或雷雨: '暴雨',
-    晴短暫陣雨或雷雨: '暴雨',
-    多雲時晴短暫陣雨或雷雨: '暴雨',
-    午後短暫雷陣雨: '暴雨',
-    多雲時陰陣雨或雷雨: '暴雨',
-    多雲時陰短暫陣雨或雷雨: '暴雨',
-    多雲時陰短暫雷陣雨: '暴雨',
-    多雲時陰雷陣雨: '暴雨',
-    晴陣雨或雷雨: '暴雨',
-    晴時多雲陣雨或雷雨: '暴雨',
-    多雲時晴陣雨或雷雨: '暴雨',
-    陰時多雲有雷陣雨: '暴雨',
-    陰時多雲陣雨或雷雨: '暴雨',
-    陰時多雲短暫陣雨或雷雨: '暴雨',
-    陰時多雲短暫雷陣雨: '暴雨',
-    陰時多雲雷陣雨: '暴雨',
-    陰有陣雨或雷雨: '暴雨',
-    陰有雷陣雨: '暴雨',
-    陰陣雨或雷雨: '暴雨',
-    陰雷陣雨: '暴雨',
-    晴午後陰短暫陣雨或雷雨: '暴雨',
-    晴午後陰短暫雷陣雨: '暴雨',
-    陰短暫陣雨或雷雨: '暴雨',
-    陰短暫雷陣雨: '暴雨',
-    雷雨: '暴雨',
-    陣雨或雷雨後多雲: '暴雨',
-    陰陣雨或雷雨後多雲: '暴雨',
-    陰短暫陣雨或雷雨後多雲: '暴雨',
-    陰短暫雷陣雨後多雲: '暴雨',
-    陰雷陣雨後多雲: '暴雨',
-    雷陣雨後多雲: '暴雨',
-    陣雨或雷雨: '暴雨',
-    雷陣雨: '暴雨',
-    午後雷陣雨: '暴雨',
-    晴午後多雲局部雨: '雨',
-    晴午後多雲局部陣雨: '暴雨',
-    晴午後多雲局部短暫雨: '雨',
-    晴午後多雲局部短暫陣雨: '暴雨',
-    晴午後多雲短暫雨: '雨',
-    晴午後多雲短暫陣雨: '暴雨',
-    晴午後局部雨: '雨',
-    晴午後局部陣雨: '暴雨',
-    晴午後局部短暫雨: '雨',
-    晴午後局部短暫陣雨: '暴雨',
-    晴午後陣雨: '暴雨',
-    晴午後短暫雨: '雨',
-    晴午後短暫陣雨: '暴雨',
-    晴時多雲午後短暫陣雨: '暴雨',
-    多雲午後局部雨: '雨',
-    多雲午後局部陣雨: '暴雨',
-    多雲午後局部短暫雨: '雨',
-    多雲午後局部短暫陣雨: '暴雨',
-    多雲午後陣雨: '暴雨',
-    多雲午後短暫雨: '雨',
-    多雲午後短暫陣雨: '暴雨',
-    多雲時陰午後短暫陣雨: '暴雨',
-    陰時多雲午後短暫陣雨: '暴雨',
-    多雲時晴午後短暫陣雨: '暴雨',
-    晴午後多雲陣雨或雷雨: '暴雨',
-    晴午後多雲雷陣雨: '暴雨',
-    晴午後陣雨或雷雨: '暴雨',
-    晴午後雷陣雨: '暴雨',
-    晴午後多雲局部陣雨或雷雨: '暴雨',
-    晴午後多雲局部短暫陣雨或雷雨: '暴雨',
-    晴午後多雲局部短暫雷陣雨: '暴雨',
-    晴午後多雲局部雷陣雨: '暴雨',
-    晴午後多雲短暫陣雨或雷雨: '暴雨',
-    晴午後多雲短暫雷陣雨: '暴雨',
-    晴午後局部短暫雷陣雨: '暴雨',
-    晴午後局部雷陣雨: '暴雨',
-    晴午後短暫雷陣雨: '暴雨',
-    晴雷陣雨: '暴雨',
-    晴時多雲雷陣雨: '暴雨',
-    晴時多雲午後短暫雷陣雨: '暴雨',
-    多雲午後局部陣雨或雷雨: '暴雨',
-    多雲午後局部短暫陣雨或雷雨: '暴雨',
-    多雲午後局部短暫雷陣雨: '暴雨',
-    多雲午後局部雷陣雨: '暴雨',
-    多雲午後陣雨或雷雨: '暴雨',
-    多雲午後短暫陣雨或雷雨: '暴雨',
-    多雲午後短暫雷陣雨: '暴雨',
-    多雲午後雷陣雨: '暴雨',
-    多雲時晴雷陣雨: '暴雨',
-    多雲時晴午後短暫雷陣雨: '暴雨',
-    多雲時陰午後短暫雷陣雨: '暴雨',
-    陰時多雲午後短暫雷陣雨: '暴雨',
-    陰午後短暫雷陣雨: '暴雨',
-    多雲局部陣雨或雪: '暴雨',
-    多雲時陰有雨或雪: '雨',
-    多雲時陰短暫雨或雪: '雨',
-    多雲短暫雨或雪: '雨',
-    陰有雨或雪: '雨',
-    陰時多雲有雨或雪: '雨',
-    陰時多雲短暫雨或雪: '雨',
-    陰短暫雨或雪: '雨',
-    多雲時陰有雪: '雨',
-    多雲時陰短暫雪: '雨',
-    多雲短暫雪: '雨',
-    陰有雪: '雨',
-    陰時多雲有雪: '雨',
-    陰時多雲短暫雪: '雨',
-    陰短暫雪: '雨',
-    有雨或雪: '雨',
-    有雨或短暫雪: '雨',
-    陰有雨或短暫雪: '雨',
-    陰時多雲有雨或短暫雪: '雨',
-    多雲時陰有雨或短暫雪: '雨',
-    多雲有雨或短暫雪: '雨',
-    多雲有雨或雪: '雨',
-    多雲時晴有雨或雪: '雨',
-    晴時多雲有雨或雪: '雨',
-    晴有雨或雪: '雨',
-    短暫雨或雪: '雨',
-    多雲時晴短暫雨或雪: '雨',
-    晴時多雲短暫雨或雪: '雨',
-    晴短暫雨或雪: '雨',
-    有雪: '雨',
-    多雲有雪: '雨',
-    多雲時晴有雪: '雨',
-    晴時多雲有雪: '雨',
-    晴有雪: '雨',
-    短暫雪: '雨',
-    多雲時晴短暫雪: '雨',
-    晴時多雲短暫雪: '雨',
-    晴短暫雪: '雨',
-    晴有霧: '霧或煙',
-    晴晨霧: '霧或煙',
-    晴時多雲有霧: '霧或煙',
-    晴時多雲晨霧: '霧或煙',
-    多雲時晴有霧: '霧或煙',
-    多雲時晴晨霧: '霧或煙',
-    多雲有霧: '霧或煙',
-    多雲晨霧: '霧或煙',
-    有霧: '霧或煙',
-    晨霧: '霧或煙',
-    陰有霧: '霧或煙',
-    陰晨霧: '霧或煙',
-    多雲時陰有霧: '霧或煙',
-    多雲時陰晨霧: '霧或煙',
-    陰時多雲有霧: '霧或煙',
-    陰時多雲晨霧: '霧或煙',
-    多雲局部雨: '雨',
-    多雲局部陣雨: '暴雨',
-    多雲局部短暫雨: '雨',
-    多雲局部短暫陣雨: '暴雨',
-    多雲時陰局部雨: '雨',
-    多雲時陰局部陣雨: '暴雨',
-    多雲時陰局部短暫雨: '雨',
-    多雲時陰局部短暫陣雨: '暴雨',
-    晴午後陰局部雨: '雨',
-    晴午後陰局部陣雨: '暴雨',
-    晴午後陰局部短暫雨: '雨',
-    晴午後陰局部短暫陣雨: '暴雨',
-    陰局部雨: '雨',
-    陰局部陣雨: '暴雨',
-    陰局部短暫雨: '雨',
-    陰局部短暫陣雨: '暴雨',
-    陰時多雲局部雨: '雨',
-    陰時多雲局部陣雨: '暴雨',
-    陰時多雲局部短暫雨: '雨',
-    陰時多雲局部短暫陣雨: '暴雨',
-    多雲有霧有局部雨: '雨',
-    多雲有霧有局部陣雨: '暴雨',
-    多雲有霧有局部短暫雨: '雨',
-    多雲有霧有局部短暫陣雨: '暴雨',
-    多雲有霧有陣雨: '暴雨',
-    多雲有霧有短暫雨: '雨',
-    多雲有霧有短暫陣雨: '暴雨',
-    多雲局部雨有霧: '雨',
-    多雲局部雨晨霧: '雨',
-    多雲局部陣雨有霧: '暴雨',
-    多雲局部陣雨晨霧: '暴雨',
-    多雲局部短暫雨有霧: '雨',
-    多雲局部短暫雨晨霧: '雨',
-    多雲局部短暫陣雨有霧: '暴雨',
-    多雲局部短暫陣雨晨霧: '暴雨',
-    多雲陣雨有霧: '暴雨',
-    多雲短暫雨有霧: '雨',
-    多雲短暫雨晨霧: '雨',
-    多雲短暫陣雨有霧: '暴雨',
-    多雲短暫陣雨晨霧: '暴雨',
-    有霧有短暫雨: '雨',
-    有霧有短暫陣雨: '暴雨',
-    多雲時陰有霧有局部雨: '雨',
-    多雲時陰有霧有局部陣雨: '暴雨',
-    多雲時陰有霧有局部短暫雨: '雨',
-    多雲時陰有霧有局部短暫陣雨: '暴雨',
-    多雲時陰有霧有陣雨: '暴雨',
-    多雲時陰有霧有短暫雨: '雨',
-    多雲時陰有霧有短暫陣雨: '暴雨',
-    多雲時陰局部雨有霧: '雨',
-    多雲時陰局部陣雨有霧: '暴雨',
-    多雲時陰局部短暫雨有霧: '雨',
-    多雲時陰局部短暫陣雨有霧: '暴雨',
-    多雲時陰陣雨有霧: '雨',
-    多雲時陰短暫雨有霧: '雨',
-    多雲時陰短暫雨晨霧: '雨',
-    多雲時陰短暫陣雨有霧: '暴雨',
-    多雲時陰短暫陣雨晨霧: '暴雨',
-    陰有霧有陣雨: '暴雨',
-    陰局部雨有霧: '雨',
-    陰局部陣雨有霧: '暴雨',
-    陰局部短暫陣雨有霧: '暴雨',
-    陰時多雲有霧有局部雨: '雨',
-    陰時多雲有霧有局部陣雨: '暴雨',
-    陰時多雲有霧有局部短暫雨: '雨',
-    陰時多雲有霧有局部短暫陣雨: '暴雨',
-    陰時多雲有霧有陣雨: '暴雨',
-    陰時多雲有霧有短暫雨: '雨',
-    陰時多雲有霧有短暫陣雨: '暴雨',
-    陰時多雲局部雨有霧: '雨',
-    陰時多雲局部陣雨有霧: '暴雨',
-    陰時多雲局部短暫雨有霧: '雨',
-    陰時多雲局部短暫陣雨有霧: '暴雨',
-    陰時多雲陣雨有霧: '暴雨',
-    陰時多雲短暫雨有霧: '雨',
-    陰時多雲短暫雨晨霧: '雨',
-    陰時多雲短暫陣雨有霧: '暴雨',
-    陰時多雲短暫陣雨晨霧: '暴雨',
-    陰陣雨有霧: '暴雨',
-    陰短暫雨有霧: '雨',
-    陰短暫雨晨霧: '雨',
-    陰短暫陣雨有霧: '暴雨',
-    陰短暫陣雨晨霧: '暴雨',
-    多雲局部陣雨或雷雨: '暴雨',
-    多雲局部短暫陣雨或雷雨: '暴雨',
-    多雲局部短暫雷陣雨: '暴雨',
-    多雲局部雷陣雨: '暴雨',
-    多雲時陰局部陣雨或雷雨: '暴雨',
-    多雲時陰局部短暫陣雨或雷雨: '暴雨',
-    多雲時陰局部短暫雷陣雨: '暴雨',
-    多雲時陰局部雷陣雨: '暴雨',
-    晴午後陰局部陣雨或雷雨: '暴雨',
-    晴午後陰局部短暫陣雨或雷雨: '暴雨',
-    晴午後陰局部短暫雷陣雨: '暴雨',
-    晴午後陰局部雷陣雨: '暴雨',
-    陰局部陣雨或雷雨: '暴雨',
-    陰局部短暫陣雨或雷雨: '暴雨',
-    陰局部短暫雷陣雨: '暴雨',
-    陰局部雷陣雨: '暴雨',
-    陰時多雲局部陣雨或雷雨: '暴雨',
-    陰時多雲局部短暫陣雨或雷雨: '暴雨',
-    陰時多雲局部短暫雷陣雨: '暴雨',
-    陰時多雲局部雷陣雨: '暴雨',
-    多雲有陣雨或雷雨有霧: '暴雨',
-    多雲有雷陣雨有霧: '暴雨',
-    多雲有霧有陣雨或雷雨: '暴雨',
-    多雲有霧有雷陣雨: '暴雨',
-    多雲局部陣雨或雷雨有霧: '暴雨',
-    多雲局部短暫陣雨或雷雨有霧: '暴雨',
-    多雲局部短暫雷陣雨有霧: '暴雨',
-    多雲局部雷陣雨有霧: '暴雨',
-    多雲陣雨或雷雨有霧: '暴雨',
-    多雲短暫陣雨或雷雨有霧: '暴雨',
-    多雲短暫雷陣雨有霧: '暴雨',
-    多雲雷陣雨有霧: '暴雨',
-    多雲時晴短暫陣雨或雷雨有霧: '暴雨',
-    多雲時陰有陣雨或雷雨有霧: '暴雨',
-    多雲時陰有雷陣雨有霧: '暴雨',
-    多雲時陰有霧有陣雨或雷雨: '暴雨',
-    多雲時陰有霧有雷陣雨: '暴雨',
-    多雲時陰局部陣雨或雷雨有霧: '暴雨',
-    多雲時陰局部短暫陣雨或雷雨有霧: '暴雨',
-    多雲時陰局部短暫雷陣雨有霧: '暴雨',
-    多雲時陰局部雷陣雨有霧: '暴雨',
-    多雲時陰陣雨或雷雨有霧: '暴雨',
-    多雲時陰短暫陣雨或雷雨有霧: '暴雨',
-    多雲時陰短暫雷陣雨有霧: '暴雨',
-    多雲時陰雷陣雨有霧: '暴雨',
-    陰局部陣雨或雷雨有霧: '暴雨',
-    陰局部短暫陣雨或雷雨有霧: '暴雨',
-    陰局部短暫雷陣雨有霧: '暴雨',
-    陰局部雷陣雨有霧: '暴雨',
-    陰時多雲有陣雨或雷雨有霧: '暴雨',
-    陰時多雲有雷陣雨有霧: '暴雨',
-    陰時多雲有霧有陣雨或雷雨: '暴雨',
-    陰時多雲有霧有雷陣雨: '暴雨',
-    陰時多雲局部陣雨或雷雨有霧: '暴雨',
-    陰時多雲局部短暫陣雨或雷雨有霧: '暴雨',
-    陰時多雲局部短暫雷陣雨有霧: '暴雨',
-    陰時多雲局部雷陣雨有霧: '暴雨',
-    陰時多雲陣雨或雷雨有霧: '暴雨',
-    陰時多雲短暫陣雨或雷雨有霧: '暴雨',
-    陰時多雲短暫雷陣雨有霧: '暴雨',
-    陰時多雲雷陣雨有霧: '暴雨',
-    陰短暫陣雨或雷雨有霧: '暴雨',
-    陰短暫雷陣雨有霧: '暴雨',
-    雷陣雨有霧: '暴雨',
-    多雲局部雨或雪有霧: '雨',
-    多雲時陰局部雨或雪有霧: '雨',
-    陰時多雲局部雨或雪有霧: '雨',
-    陰局部雨或雪有霧: '雨',
-    短暫雨或雪有霧: '雨',
-    有雨或雪有霧: '雨',
-    短暫陣雨有霧: '暴雨',
-    短暫陣雨晨霧: '暴雨',
-    短暫雨有霧: '雨',
-    短暫雨晨霧: '雨',
-    有雨有霧: '雨',
-    陣雨有霧: '雨',
-    短暫陣雨或雷雨有霧: '暴雨',
-    陣雨或雷雨有霧: '暴雨',
-    下雪: '雨',
-    積冰: '雨',
-    暴風雪: '雨',
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === 'origin') {
-      setSearchText1(e.target.value);
-    } else {
-      setSearchText2(e.target.value);
-    }
-  };
-
-  const submit = async () => {
+  async function submit() {
     if (searchText1 === '' || searchText2 === '') {
       return;
     }
 
     try {
-      const apiKey = process.env.REACT_APP_API_KEY;
-      const geocodingUrl1 = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-        searchText1
-      )}&key=${apiKey}`;
-      const geocodingUrl2 = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-        searchText2
-      )}&key=${apiKey}`;
+      // --------------- search texts -> coordinates ---------------
+      const res1 = await geocoding(searchText1);
 
-      const res1 = await axios.get(geocodingUrl1);
-      const res2 = await axios.get(geocodingUrl2);
-      const results1 = res1.data.results;
-      const results2 = res2.data.results;
-
-      if (results1.length === 0) {
+      if (!res1) {
         alert('請重新輸入起點');
         return;
       }
 
-      if (results2.length === 0) {
+      const res2 = await geocoding(searchText2);
+
+      if (!res2) {
         alert('請重新輸入終點');
         return;
       }
 
-      const newLat1 = results1[0].geometry.location.lat;
-      const newLng1 = results1[0].geometry.location.lng;
-      const newLat2 = results2[0].geometry.location.lat;
-      const newLng2 = results2[0].geometry.location.lng;
+      const lat1 = res1.lat;
+      const lng1 = res1.lng;
+      const lat2 = res2.lat;
+      const lng2 = res2.lng;
 
-      setLat1(newLat1);
-      setLng1(newLng1);
-      setLat2(newLat2);
-      setLng2(newLng2);
+      setCenterLat((lat1 + lat2) / 2);
+      setCenterLng((lng1 + lng2) / 2);
+      getRoutes(lat1, lng1, lat2, lng2);
+
+      // --------------- coordinates -> districts ---------------
+      const district1 = await reverseGeocoding(lat1, lng1);
+      const district2 = await reverseGeocoding(lat2, lng2);
+
+      if (!district1 && district2) {
+        alert('起點不在台南');
+        return;
+      }
+
+      if (district1 && !district2) {
+        alert('終點不在台南');
+        return;
+      }
+
+      if (!district1 && !district2) {
+        alert('起終點皆不在台南');
+        return;
+      }
+
+      // --------------- get weather in district 1 & 2 ---------------
+      const weather1 = await getWeather(district1);
+      const weather2 = await getWeather(district2);
+
+      // --------------- get high-risk locations ---------------
+      setIsLoading(true);
+      setSearchResults([]);
+
+      const locations = await getHighRiskLocations(
+        district1,
+        weather1,
+        district2,
+        weather2
+      );
+
+      setIsLoading(false);
+      setSearchResults(locations);
     } catch (error) {
       console.error('Geocoding error:', error);
     }
-  };
+  }
 
-  const getRoutes = () => {
-    if (!lat1 || !lng1 || !lat2 || !lng2) {
-      return;
+  async function geocoding(searchText) {
+    const apiKey = process.env.REACT_APP_MAP_API_KEY;
+    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchText}&key=${apiKey}`;
+    const res = await axios.get(geocodingUrl);
+
+    if (res.data.results.length === 0) {
+      return new Promise((resolve, reject) => {
+        resolve(null);
+      });
     }
 
+    return new Promise((resolve, reject) => {
+      resolve(res.data.results[0].geometry.location);
+    });
+  }
+
+  async function reverseGeocoding(lat, lng) {
+    try {
+      const apiKey = process.env.REACT_APP_MAP_API_KEY;
+      const reverseGeocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`;
+      const res = await axios.get(reverseGeocodingUrl);
+      const pattern = /台南市.+區/;
+      const match = res.data.plus_code.compound_code.match(pattern)?.[0];
+
+      if (match) {
+        return new Promise((resolve, reject) => {
+          resolve(match.slice(3));
+        });
+      }
+
+      return new Promise((resolve, reject) => {
+        resolve(null);
+      });
+    } catch (error) {
+      console.error('Reverse geocoding error:', error);
+    }
+  }
+
+  async function getWeather(district) {
+    try {
+      const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+      const current_time = new Date();
+      const current_hour = current_time.getHours();
+      const time = current_hour < 6 ? 0 : current_hour < 18 ? 1 : 2;
+      const weatherUrl = `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-079?Authorization=${apiKey}&elementName=WeatherDescription`;
+      const res = await axios.get(weatherUrl);
+      const data = res.data;
+      const weather = {};
+
+      for (let i = 0; i < 37; i++) {
+        const name =
+          data['records']['locations'][0]['location'][i]['locationName'];
+        const weatherElement =
+          data['records']['locations'][0]['location'][i]['weatherElement'][0][
+            'time'
+          ][time]['elementValue'][0]['value'].split('。')[0];
+        weather[name] = weatherElement;
+      }
+
+      return new Promise((resolve, reject) => {
+        resolve(simplifiedWeather[weather[district]]);
+      });
+    } catch (error) {
+      console.error('Weather error:', error);
+    }
+  }
+
+  async function getHighRiskLocations(
+    district1,
+    weather1,
+    district2,
+    weather2
+  ) {
+    try {
+      const serverURl1 = `https://traffic-accident-risk-backend.onrender.com/api/myfunction?district=${district1}&weather=${weather1}`;
+      const serverURl2 = `https://traffic-accident-risk-backend.onrender.com/api/myfunction?district=${district2}&weather=${weather2}`;
+
+      const res1 = await axios.get(serverURl1);
+      const res2 = await axios.get(serverURl2);
+
+      const seenLocations = new Set();
+      const combinedResults = [];
+
+      [...res1.data, ...res2.data].forEach((result) => {
+        if (!seenLocations.has(result.location)) {
+          combinedResults.push(result);
+          seenLocations.add(result.location);
+        }
+      });
+
+      return new Promise((resolve, reject) => {
+        resolve(combinedResults);
+      });
+    } catch (error) {
+      console.error('Request error', error);
+    }
+  }
+
+  function getRoutes(lat1, lng1, lat2, lng2) {
     const directionsService = new window.google.maps.DirectionsService();
 
     const origin = new window.google.maps.LatLng(lat1, lng1);
@@ -449,116 +205,27 @@ function Home() {
         setDirections(result);
       }
     });
-  };
+  }
 
-  const reverseGeocoding = async () => {
-    try {
-      const apiKey = process.env.REACT_APP_API_KEY;
-      const reverseGeocodingUrl1 = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat1},${lng1}&key=${apiKey}`;
-      const reverseGeocodingUrl2 = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat2},${lng2}&key=${apiKey}`;
-      const res1 = await axios.get(reverseGeocodingUrl1);
-      const res2 = await axios.get(reverseGeocodingUrl2);
-      const pattern = /台南市.+區/;
-      const match1 = res1.data.plus_code.compound_code.match(pattern)?.[0];
-      const match2 = res2.data.plus_code.compound_code.match(pattern)?.[0];
-
-      if (match1 && match2) {
-        const district1 = match1.slice(3);
-        const district2 = match2.slice(3);
-
-        getWeather(district1, district2);
-      }
-    } catch (error) {
-      console.error('Reverse geocoding error:', error);
+  function handleChange(e) {
+    if (e.target.name === 'origin') {
+      setSearchText1(e.target.value);
+    } else {
+      setSearchText2(e.target.value);
     }
-  };
+  }
 
-  const getWeather = async (district1, district2) => {
-    try {
-      setIsLoading(true);
-
-      const current_time = new Date();
-      const current_hour = current_time.getHours();
-      const time = current_hour < 6 ? 0 : current_hour < 18 ? 1 : 2;
-      const weatherUrl =
-        'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-079?Authorization=CWB-C8017091-8B5F-4A12-AB41-E99EF815C107&elementName=WeatherDescription';
-
-      const res = await axios.get(weatherUrl);
-      const data = res.data;
-      const weather = {};
-
-      for (let i = 0; i < 37; i++) {
-        const name =
-          data['records']['locations'][0]['location'][i]['locationName'];
-        const weatherElement =
-          data['records']['locations'][0]['location'][i]['weatherElement'][0][
-            'time'
-          ][time]['elementValue'][0]['value'].split('。')[0];
-        weather[name] = weatherElement;
-      }
-
-      sendRequest(
-        district1,
-        simplifiedWeather[weather[district1]],
-        district2,
-        simplifiedWeather[weather[district2]]
-      );
-    } catch (error) {
-      console.error('Weather error:', error);
-    }
-  };
-
-  const sendRequest = async (district1, weather1, district2, weather2) => {
-    setSearchResults([]);
-
-    try {
-      const backendUrl1 = `https://traffic-accident-risk-backend.onrender.com/api/myfunction?district=${district1}&weather=${weather1}`;
-      const backendUrl2 = `https://traffic-accident-risk-backend.onrender.com/api/myfunction?district=${district2}&weather=${weather2}`;
-
-      const res1 = await axios.get(backendUrl1);
-      const res2 = await axios.get(backendUrl2);
-
-      const seenLocations = new Set();
-      const combinedResults = [];
-
-      [...res1.data, ...res2.data].forEach((result) => {
-        if (!seenLocations.has(result.location)) {
-          combinedResults.push(result);
-          seenLocations.add(result.location);
-        }
-      });
-
-      setIsLoading(false);
-      setSearchResults(combinedResults);
-    } catch (error) {
-      console.error('Request error', error);
-    }
-  };
-
-  const reset = () => {
+  function reset() {
     setSearchText1('');
     setSearchText2('');
     setSearchResults([]);
     setDirections(null);
-  };
+  }
 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY,
     libraries: ['places'],
   });
-
-  useEffect(() => {
-    if (!lat1 || !lng1 || !lat2 || !lng2) {
-      return;
-    }
-
-    setCenterLat((lat1 + lat2) / 2);
-    setCenterLng((lng1 + lng2) / 2);
-    getRoutes();
-
-    // Order: reverseGeocoding -> getWeather -> sendRequest
-    reverseGeocoding();
-  }, [lat1, lng1, lat2, lng2]);
 
   useEffect(() => {
     // for weather widget
